ebac6661a720c529008deec67fbd5d919da0c91d
Danylo Semankiv <143294027+DanyloSem@users.noreply.github.com>
2024-05-26 19:30:45 +0000

    Ready title search, Max 25 songs

--- api_tests/title_search.py
+++ api_tests/title_search.py
@@ -6,7 +6,6 @@ sys.path.append('..')
 from config import APP_ID, SECRET
 
 TITLE_SEARCH_URL = 'https://api.planningcenteronline.com/services/v2/songs?where[title]='
-URL_FOR_SONG_ID = 'https://api.planningcenteronline.com/services/v2/songs/'
 
 app_id = APP_ID
 secret = SECRET
@@ -21,8 +20,7 @@ headers = {
 }
 
 def get_response_json(request_url, headers):
-    complete_url = ''.join([request_url, user_input])
-    response = requests.get(complete_url, headers=headers)
+    response = requests.get(request_url, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
@@ -30,34 +28,33 @@ def get_response_json(request_url, headers):
 
 def get_songs_dict(data):
     songs_dict = {}
+    index = 0
     for song in data['data']:
         song_title = song['attributes']['title']
-        song_id = song['id']
-        songs_dict[song_title] = song_id
+        song_url = song['links']['self']
+        index += 1
+        songs_dict[index] = {
+            'title': song_title,
+            'url': song_url
+        }
     return songs_dict
 
 def choose_song(songs_dict):
-    [print(f"{i + 1}. {song_title}") for i, song_title in enumerate(songs_dict)]
+    [print(f"{index}. {song_atrs['title']}") for index, song_atrs in songs_dict.items()]
     chosen_song = int(input('Введи номер вибраної пісні: '))
-    print(chosen_song)
-    chosen_song_id = list(songs_dict.values())[chosen_song]
-    print(chosen_song_id)
-    return chosen_song_id
+    return chosen_song
 
-def get_song_text(data):
-    lyrics = data['data']['attributes']['lyrics']
+def get_song_text(song_data):
+    lyrics = song_data['data'][0]['attributes']['lyrics']
     print(lyrics)
 
 
-title_search_data = get_response_json(TITLE_SEARCH_URL, headers)
-if title_search_data is not None:
-    songs_dict = get_songs_dict(title_search_data)
-    chosen_song_id = choose_song(songs_dict)
-    if chosen_song_id is not None:
-        chosen_song_url = ''.join([URL_FOR_SONG_ID, chosen_song_id, '/arrangements'])
-        print(chosen_song_url)
-        song_search_data = get_response_json(chosen_song_url, headers)
-        if song_search_data is not None:
-            get_song_text(song_search_data)
+complete_search_url = ''.join([TITLE_SEARCH_URL, user_input])
+title_search_data = get_response_json(complete_search_url, headers)
+songs_dict = get_songs_dict(title_search_data)
+chosen_song = choose_song(songs_dict)
+chosen_song_url = ''.join([songs_dict[chosen_song]['url'], '/arrangements'])
+song_data = get_response_json(chosen_song_url, headers)
+get_song_text(song_data)
 
 
